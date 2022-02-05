d91c3b01b07adf99fb2f7230eb71759404891e34
Gwin_disel <danya.savin.03@mail.ru>
2022-02-05 21:07:38 +0300

    memory usage of vector

--- main.c
+++ main.c
@@ -1,3 +1,10 @@
+#include <stdio.h>
+#include <stdint.h>
+#include "libs/data_structures/vector/vector.h"
+
 int main() {
+    vector a = createVector(SIZE_MAX);
+
+    printf("%u", SIZE_MAX);
     return 0;
 }
